c43608a53cea7e818b37f38f013c4cd189bb1934
Spacedunk <spacedunk@gmail.com>
2016-11-17 14:52:32 -0500

    Added proper constructor Moved data retreival to its own function instead of Coat function

--- app/Shabbos_Weather.js
+++ app/Shabbos_Weather.js
@@ -3,21 +3,31 @@ var forecast = require('./forecast.js');
 var forecast_test = require('./forecast.test.js');
 
 //Gathers Weather Information and Determines Whether (no pun intended) a Coat or Jacket will be needed in the given timespan
-var WeatherData;
 
-module.exports.DoINeedACoat = function(lat,long,callback)
+Shabbos_Weather.prototype.DoINeedACoat = function(lat,long,callback)
 {
-	 if(process.env.ENVIRONMENT != 'DEV')
+	callback(false);
+}
+
+function Shabbos_Weather(temp_threashhold,hours_forward) {
+	this.temprature = temp_threashhold;
+	this.hours_ahead = hours_forward;
+};
+
+Shabbos_Weather.prototype.GetData = function(callback)
+{
+	if(process.env.ENVIRONMENT != 'DEV')
 	 {
 	 	forecast.GetHourlyWeatherData(lat , long, function(weather_data){
-	 		WeatherData = weather_data;
+	 		callback(weather_data);
 	 	});
 	 }
 	 else
 	 {
 	 	forecast_test.ImportWeatherInfoFromCSV('',function(weather_data) {
-	 		WeatherData = weather_data;
-	 		callback(WeatherData);
+	 		callback(weather_data);
 	 	});
 	 }
 }
+
+module.exports.Shabbos_Weather = Shabbos_Weather;
