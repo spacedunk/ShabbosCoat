4bebfaf55036753c721058f2e758b0d78cc6422f
Spacedunk <spacedunk@gmail.com>
2017-11-09 10:11:37 -0500

    Changed  changed long to longitude and lat to latitude

--- controller/ShabbosWeatherController.js
+++ controller/ShabbosWeatherController.js
@@ -2,14 +2,15 @@ var s_weather = require('../app/Shabbos_Weather.js');
 exports.DoINeedACoat = function(req,res) {
     var json_data = JSON.parse(req.params.data);
     var sw = new s_weather.Shabbos_Weather(json_data.threshhold,json_data.hours,json_data.options);
-        sw.DoINeedACoat(json_data.lat,json_data.long,function(coat){
+        sw.DoINeedACoat(json_data.latitude,json_data.longitude,function(coat){
             res.json({ coat : coat});
         });
 };
 
 exports.DoINeedACoat_Default = function(req,res) {
+    var json_data = JSON.parse(req.params.data);
     var sw = new s_weather.Shabbos_Weather(process.env.DEFAULT_TEMP_THRESHHOLD,process.env.DEFAULT_HOURS);
-        sw.DoINeedACoat(req.params.lat,req.params.long,function(coat){
+        sw.DoINeedACoat(json_data.latitude,json_data.longitude,function(coat){
             res.json({ coat : coat});
     });
 };
